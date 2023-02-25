aeca08968e4469859aaab6298a9dbc3344f7288c
ShivaniAnbu5 <125631700+ShivaniAnbu5@users.noreply.github.com>
2023-02-25 23:54:46 +0530

    Update ex15a_prime_no_list.java

--- ex15a_prime_no_list.java
+++ ex15a_prime_no_list.java
@@ -4,15 +4,18 @@ public class ex15a_prime_no_list {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1;i<=100;i++) {
-			int divi=0;
-			for(int j=1;j<=i/2;j++) {
-				if(i%j==0) divi++; 
+		boolean flag=false;
+		for(int i=2;i<=100;i++) {
+			flag=false;
+			for(int j=2;j<=i/2;j++) {
+				if(i%j==0) 
+					{flag=true; 
 //				System.out.println("divi is "+divi);
-				if(divi>1) break;
+				 break;
+					}
 				
 			}
-			if(divi == 1) System.out.print(i+" ");
+			if(flag == false) System.out.print(i+" ");
 		}
 	}
 
