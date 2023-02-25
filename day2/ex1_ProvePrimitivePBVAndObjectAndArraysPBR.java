f0a87b0517c83f7001661e116561455fab805a83
ShivaniAnbu5 <125631700+ShivaniAnbu5@users.noreply.github.com>
2023-02-25 23:58:10 +0530

    Update ex1_ProvePrimitivePBVAndObjectAndArraysPBR.java

--- day2/ex1_ProvePrimitivePBVAndObjectAndArraysPBR.java
+++ day2/ex1_ProvePrimitivePBVAndObjectAndArraysPBR.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import java.util.Arrays;
+
 public class ex1_ProvePrimitivePBVAndObjectAndArraysPBR {
 
 	
@@ -7,13 +9,14 @@ public class ex1_ProvePrimitivePBVAndObjectAndArraysPBR {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int value = 5;
+		int arr[] = {4,5,6};
 		subclass obj = new subclass();
-		
+
 		obj.printPBV(value);
 		System.out.println("After whole PBV execution value is "+value);
 		
-		obj.printPBR(obj);
-		System.out.println("After whole PBR execution value is "+obj.referenceValue);
+		obj.printPBR(arr);
+		System.out.println("After whole PBR execution value is "+Arrays.toString(arr));
 		
 	}
 	
@@ -21,8 +24,11 @@ public class ex1_ProvePrimitivePBVAndObjectAndArraysPBR {
 		System.out.println("Before PBV, value is "+val);
 	}
 	
-	void printPBR(subclass obj) {
-		System.out.println("Before PBR, value is "+ obj.referenceValue);
+//	void printPBR(subclass obj) {
+//		System.out.println("Before PBR, value is "+ obj.referenceValue);
+//	}
+	void printPBR(int[] arr) {
+		System.out.println("Before PBR, value is "+ Arrays.toString(arr));
 	}
 
 }
@@ -36,19 +42,26 @@ class subclass extends ex1_ProvePrimitivePBVAndObjectAndArraysPBR{
 		// TODO Auto-generated method stub
 		super.printPBV(val);
 		referenceValue = 100;
-		System.out.println("After PBV execution value is "+referenceValue);
+		System.out.println("After PBV execution, value is "+referenceValue);
 	}
 
+//	@Override
+//	void printPBR(subclass obj) {
+//		// TODO Auto-generated method stub
+//		super.printPBR(obj);
+//		obj.referenceValue = 6587;
+//		System.out.println("After PBR execution value is "+obj.referenceValue);
+//	}
+
+	
 	@Override
-	void printPBR(subclass obj) {
+	void printPBR(int[] arr) {
+		arr = new int[5];
 		// TODO Auto-generated method stub
-		super.printPBR(obj);
-		obj.referenceValue = 6587;
-		System.out.println("After PBR execution value is "+obj.referenceValue);
+		super.printPBR(arr);
+		arr[2] = 4;
+		System.out.println("After PBR execution, value is "+Arrays.toString(arr));
 	}
-
-	
-	
 	
 	
 	
