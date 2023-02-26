d7d9a151f5ec53a9221fbdbb00b1c4e1412b2bf0
ShivaniAnbu5 <shivani.anbumozhi@cdw.com>
2023-02-26 18:59:14 +0530

    Update Ex8NumberFormatException.java

--- day4/Ex8NumberFormatException.java
+++ day4/Ex8NumberFormatException.java
@@ -3,31 +3,29 @@ package exercises;
 import java.util.Scanner;
 
 public class Ex8NumberFormatException {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		String number;
-		int num,average=0,sum=0;
+		int num, average = 0, sum = 0;
 		System.out.println("Enter the marks for 10 students:");
 		int studentCount = 0;
-		while(studentCount != 10) {
-			System.out.println("Enter marks for student "+(studentCount+1)+":");
+		while (studentCount != 10) {
+			System.out.println("Enter marks for student " + (studentCount + 1) + ":");
 			number = s.nextLine();
 			try {
-				num=Integer.parseInt(number);
+				num = Integer.parseInt(number);
 				studentCount++;
-				
-			}
-			catch(NumberFormatException ne) {
+
+			} catch (NumberFormatException ne) {
 				System.out.println("NumberFormatException....Enter a valid number!!");
 				continue;
 			}
-			sum+=num;
+			sum += num;
 		}
-		average = sum/10;
-		System.out.println("Average mark is "+average); 
+		average = sum / 10;
+		System.out.println("Average mark is " + average);
 
-}
+	}
 
 }
